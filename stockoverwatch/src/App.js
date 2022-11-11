83981c3b5eac60bc8c19ae1dea89d8d7b89ffc71
Chris Bowyer <100442597+Devcab2@users.noreply.github.com>
2022-11-10 18:18:53 -0700

    connect app.js to server

--- stockoverwatch/src/App.js
+++ stockoverwatch/src/App.js
@@ -1,10 +1,32 @@
+import { useState,useEffect } from 'react';
 import './App.css';
+import {db} from './firebase-config';
+import {collection, getDocs} from 'firebase/firestore'
 
 function App() {
+  const [users, setUsers] = useState([]);
+    const userCollectionRef = collection(db, "users" )
+    
+    useEffect(() => {
+      const getUser= async ()=>{
+        try {const data = await getDocs(userCollectionRef)
+        setUsers(data.docs.map((doc)=>({...doc.data(),id:doc.id})))
+      } catch(err){console.log(err)}}
+
+      getUser();
+    },[]);
   return (
-    <div className="App">
+    
+   <div className="App">
+    {users.map((user)=>{
+      return(
+<div key={user.id}>
+<h1>Name: {user.name}</h1>
+<h1>Phone: {user.phone}</h1>
+</div>
+      )
+    })}
     </div>
-  );
-}
+  )}
 
 export default App;
