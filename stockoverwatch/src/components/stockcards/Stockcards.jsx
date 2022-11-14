ed63e8d78e284bf94ea986749dd298d63d3a9014
LIZXP <Peter.xli@outlook.com>
2022-11-13 22:58:21 -0700

    create stock-cards display on the top

--- stockoverwatch/src/components/stockcards/Stockcards.jsx
+++ stockoverwatch/src/components/stockcards/Stockcards.jsx
@@ -1,22 +1,37 @@
 import React from "react";
 import "./Stockcards.styles.scss";
 function Stockcards({ stocks }) {
-  const findTopThreeStocks = (arr) => {
+  const findTopFourStocks = (arr) => {
     return arr
       .sort((a, b) => {
         return a.o - a.c - (b.o - b.c);
       })
       .slice(0, 4);
   };
-  console.log(findTopThreeStocks(stocks));
+  const topFour = findTopFourStocks(stocks);
+  const diffNum = (opening, current) => {
+    return (opening - current).toFixed(2);
+  };
   return (
     <div className="Stockcards-container">
-      <div>
-        <span>Stockcards1</span>
-      </div>
-      <div>Stockcards2</div>
-      <div>Stockcards3</div>
-      <div>Stockcards4</div>
+      {topFour.map((stock) => {
+        return (
+          <div key={stock.symbol}>
+            <div className="stock-header">
+              <span>{stock.symbol}</span>
+              <span>{stock.c} USD</span>
+            </div>
+            <div className="stock-footer">
+              {diffNum(stock.o, stock.c) > 0 ? (
+                <span>nice</span>
+              ) : (
+                <span>notnice</span>
+              )}
+              <span>today</span>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
