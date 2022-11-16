54c2b0ccf20a64d97799c09be7f4348fe805b9cb
LIZXP <Peter.xli@outlook.com>
2022-11-16 13:50:49 -0700

    import image and adjust styles

--- stockoverwatch/src/components/bodycontent/BodyTwo.jsx
+++ stockoverwatch/src/components/bodycontent/BodyTwo.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import "./bodyTwo.styles.scss";
 
 function BodyTwo() {
   return (
     <div className="bodytwo-container">
-      <div className="big-title"></div>
-      <div className="left-text"></div>
-      <div className="right-image"></div>
+      <div className="big-title">
+        <h1>Investing</h1>
+        <h1>never this easy</h1>
+      </div>
+      <div className="left-text">
+        <h2>Trade thousands of stocks at your finger tip, commission-free</h2>
+      </div>
+      <div className="right-image">
+        <img src="" alt="" />
+      </div>
     </div>
   );
 }
