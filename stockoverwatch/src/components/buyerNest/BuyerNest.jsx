00df58dba0d862a78d0c564108059a95d6ea69fe
LIZXP <Peter.xli@outlook.com>
2022-11-17 11:29:59 -0700

    fix main page styling

--- stockoverwatch/src/components/buyerNest/BuyerNest.jsx
+++ stockoverwatch/src/components/buyerNest/BuyerNest.jsx
@@ -1,64 +1,11 @@
 import React from "react";
-
+import "./buyerNest.styles.scss";
 function BuyerNest() {
   return (
-    <div className="BuyerNest-container">
-      <div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-          tempora rerum earum reiciendis temporibus illum assumenda aliquid
-          reprehenderit vero nesciunt magnam, eligendi in. Odio reprehenderit
-          sed nam repudiandae deserunt alias. Possimus ad quibusdam, temporibus
-          quam sequi quisquam rem harum quis dolorum nesciunt doloremque quae
-          facere sapiente eligendi incidunt soluta et. Accusantium modi possimus
-          velit, sint reiciendis consequuntur earum totam a. Numquam, harum!
-          Alias deserunt provident eaque. Similique, eos necessitatibus dolore
-          aperiam harum consequatur amet vel ratione sint explicabo libero quam!
-          Non mollitia rerum nulla distinctio, saepe exercitationem vel quidem
-          soluta. Dolorum repellat totam magnam veritatis officiis earum
-          expedita nisi maiores quo ipsam eligendi recusandae praesentium
-          voluptatem, pariatur aut dolor! Cumque sequi quasi sed, inventore
-          dolores illum ab voluptatibus voluptatem ullam. Corporis, nesciunt
-          accusantium a distinctio ex laborum, esse, aperiam magnam eveniet
-          iusto incidunt expedita asperiores rerum illum unde itaque. Iusto
-          expedita accusamus sunt repellendus minus ea unde deleniti amet iure!
-          Dolorum enim doloremque ut delectus facilis vel incidunt id, iusto,
-          voluptates et labore! Libero unde aliquam, ullam, nisi eum laboriosam
-          ex deleniti magnam possimus sint saepe et reprehenderit eaque
-          recusandae. Corrupti rerum impedit optio quisquam accusamus quis!
-          Necessitatibus repellendus consectetur mollitia quaerat nisi unde
-          possimus, culpa omnis eaque, tempora quos. Tenetur nobis obcaecati
-          possimus, fugiat aut blanditiis tempora sint consectetur. Repudiandae
-          in laboriosam reiciendis? Mollitia, suscipit harum. Vero natus
-          officiis libero quos, molestias nesciunt magni reiciendis, porro minus
-          cupiditate accusamus praesentium. Illo ducimus necessitatibus omnis
-          quibusdam harum assumenda iure dolor? Temporibus animi velit, alias
-          consectetur dolorum provident non ea delectus quod modi dolorem itaque
-          vel aperiam quasi soluta. Consectetur, alias voluptates. Laudantium
-          consequuntur quae voluptatum rem neque quam corrupti ratione. Officia
-          magnam odio voluptas vitae soluta distinctio suscipit. Porro
-          dignissimos dolor, nostrum quo eum deserunt eligendi minus odio quae,
-          recusandae adipisci, nisi impedit delectus dolore veritatis dicta
-          consectetur tenetur aliquam. Deserunt, earum voluptates soluta sunt
-          quia perspiciatis labore numquam sequi distinctio. Pariatur veritatis
-          ex delectus, possimus praesentium dolore numquam facilis quis corporis
-          quisquam sit itaque laboriosam cupiditate harum doloribus expedita?
-          Nulla beatae autem laborum magnam dicta in laboriosam dolorum est quo
-          nemo, sit excepturi quod omnis. Sint necessitatibus illo ut nobis hic!
-          Quos consectetur voluptatibus obcaecati possimus pariatur sapiente
-          quis. Distinctio reprehenderit aperiam rerum? Eos atque dolorum sit
-          ea, veniam ipsa, excepturi consequuntur quae, a delectus odit id
-          molestiae veritatis sapiente nostrum alias saepe exercitationem! Ipsam
-          molestiae pariatur necessitatibus dignissimos! Minus cumque error
-          reiciendis similique numquam quas eveniet dolore soluta quod sunt
-          voluptate porro ad, officia perspiciatis recusandae a odit animi
-          reprehenderit veritatis aspernatur laborum consectetur eaque! Quaerat,
-          beatae libero. Debitis quaerat nesciunt ullam in ad quas cumque, magni
-          quam maiores eligendi odit quisquam accusantium modi architecto
-          adipisci quibusdam temporibus possimus officia! Ab perspiciatis
-          dignissimos ullam at, quae delectus pariatur.
-        </p>
-      </div>
+    <div className="User-Profile">
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi tempora
+      </h1>
     </div>
   );
 }
