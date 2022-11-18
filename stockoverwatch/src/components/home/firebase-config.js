94b110a02796d41b3308e909bcdc5f22706bcfd5
LIZXP <Peter.xli@outlook.com>
2022-11-18 12:27:14 -0700

    add historical stock prices Json file to the folder for firebase

--- stockoverwatch/src/components/home/firebase-config.js
+++ stockoverwatch/src/components/home/firebase-config.js
@@ -3,12 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDDvriRGfQIKvn_itfFe1CSax4g2lXegmE",
+  authDomain: "stockoverwatch.firebaseapp.com",
+  projectId: "stockoverwatch",
+  storageBucket: "stockoverwatch.appspot.com",
+  messagingSenderId: "103121083504",
+  appId: "1:103121083504:web:d02198116a7667f211a8b5",
+  measurementId: "G-M4KNYFFLQW",
 };
 
 // Initialize Firebase
