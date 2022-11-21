b7b35d0a3abcf4527f28e238a9bb27a881606521
LIZXP <Peter.xli@outlook.com>
2022-11-21 09:31:53 -0700

    create stock price list on page body

--- stockoverwatch/src/components/buyerNest/stockList/StockList.jsx
+++ stockoverwatch/src/components/buyerNest/stockList/StockList.jsx
@@ -1,41 +1,29 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./StockList.styles.scss";
 function StockList({ stocks, monthlyPrice }) {
   console.log("monthly price", monthlyPrice);
   console.log("current price", stocks);
   return (
     <div className="StockList">
-      <h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi tempora
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur
-        numquam magni dicta quam corporis, sunt nobis aliquid quod vero optio
-        quae minima atque maxime voluptatem molestias. Atque tenetur unde ullam!
-        Dolorem, quos. Illum voluptate impedit aliquid, numquam corporis natus?
-        Minus perferendis eveniet harum fuga neque eligendi dignissimos quia
-        sed? Rerum, qui unde odit deserunt explicabo similique dolore nulla
-        repellendus dolores. Doloribus culpa inventore repudiandae tempora
-        beatae! Molestiae ab eos fugit qui quos adipisci non quo illum nostrum
-        iusto eius optio, nesciunt repudiandae atque dolor ipsum vel ducimus
-        dolorum incidunt facilis. Incidunt possimus optio et laudantium est, vel
-        ducimus adipisci earum delectus iusto omnis placeat molestias,
-        perferendis voluptates tempora commodi. Modi pariatur facere, vitae quam
-        praesentium quo nihil fugit voluptas neque? In excepturi similique id
-        tempora maxime rem ut quis dolor quaerat non fuga, velit dolore
-        accusamus et nobis natus voluptatibus esse magni fugit, voluptas optio
-        doloremque. Quibusdam nobis facere excepturi. Velit cum voluptatum
-        earum. Quo at quos enim ducimus voluptatibus autem incidunt quis sequi
-        voluptas. Dolorum, iure suscipit necessitatibus accusantium velit illo
-        voluptates, facilis adipisci corrupti nemo rem corporis possimus.
-        Corrupti corporis, accusantium consequatur, aliquid nisi explicabo, vero
-        doloremque numquam maxime odit excepturi. Ratione pariatur porro, modi
-        doloremque quod esse, expedita distinctio amet harum eos veniam aliquam,
-        quidem vitae hic? Recusandae veniam delectus quia unde assumenda, magni,
-        id fugit veritatis architecto repellat eligendi illum eveniet accusamus
-        dicta! Dolor nemo fuga, reprehenderit atque at recusandae voluptates
-        ipsa ex voluptas itaque adipisci. Laudantium sit repudiandae nemo
-        tenetur earum quidem quia necessitatibus, ea laboriosam quo natus iusto
-        officia, minima amet maiores aliquam dicta. Vero est repellat harum et
-      </h1>
+      <div className="current-price">
+        <ul>
+          {stocks.map((stock, i) => {
+            return (
+              <Fragment key={i}>
+                <li>{stock.symbol}</li>
+                <li>Current price: {stock.c}</li>
+                <li>Change: {stock.d}</li>
+                <li>Percent change: {(stock.dp * 10).toFixed(2)}%</li>
+                <li>High price: {stock.h}</li>
+                <li>Low price: {stock.l}</li>
+                <li>Open price: {stock.o}</li>
+                <li>Previous close price: {stock.pc}</li>
+              </Fragment>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="monthly-price"></div>
     </div>
   );
 }
