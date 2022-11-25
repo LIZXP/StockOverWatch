fe1f8eb95ff9c90a8992ae5d8daa713d94dc23d5
LIZXP <Peter.xli@outlook.com>
2022-11-25 16:20:42 -0700

    fix some error

--- stockoverwatch/src/components/home/Home.jsx
+++ stockoverwatch/src/components/home/Home.jsx
@@ -3,17 +3,17 @@ import Body from "../bodycontent/Body";
 import "./Home.styles.scss";
 
 function Main({ stocks }) {
-  // if (!stocks) {
-  //   return null;
-  // }
-  return stocks ? (
+  if (!stocks) {
+    return null;
+  }
+  return (
     <div className="stocks-container">
       <div className="Main">
         <Navbar stocks={stocks} />
         <Body />
       </div>
     </div>
-  ) : null;
+  );
 }
 
 export default Main;
