9252d7c224f282ab4456cd97e8af47adb6d61d00
LIZXP <Peter.xli@outlook.com>
2022-11-24 21:52:54 -0700

    initialize support page

---
+++ stockoverwatch/src/components/support/Support.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./support.styles.scss";
+
+function Support() {
+  return (
+    <div className="support-container">
+      <div className="contactinfo">
+        <div>
+          <h2>Contact Info</h2>
+          <ul className="info">
+            <li>
+              <span></span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Support;
