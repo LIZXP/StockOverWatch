04c4c1e52591a24ce0b5fa22986564de7f07b4dd
LIZXP <Peter.xli@outlook.com>
2022-11-24 22:25:32 -0700

    add github and contact info for the 3 devs

--- stockoverwatch/src/components/support/Support.jsx
+++ stockoverwatch/src/components/support/Support.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./support.styles.scss";
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 function Support() {
   return (
@@ -26,6 +27,26 @@ function Support() {
                 <AlternateEmailIcon /> <br />
               </span>
               <span>Bryan.Nuguyen@gmail.com</span>
+            </li>
+          </ul>
+          <ul className="sci">
+            <li>
+              <GitHubIcon />
+            </li>
+            <li>
+              <a href="https://github.com/LIZXP">Peter Li</a>
+            </li>
+            <li>
+              <GitHubIcon />
+            </li>
+            <li>
+              <a href="https://github.com/Devcab2">Chris Bowyer</a>
+            </li>
+            <li>
+              <GitHubIcon />
+            </li>
+            <li>
+              <a href="https://github.com/dungminhnguyen257">Bryan Nguyen</a>
             </li>
           </ul>
         </div>
