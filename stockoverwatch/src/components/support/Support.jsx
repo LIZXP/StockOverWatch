f9f222b3b2cbef50bc7da1f91f719af53eb19015
LIZXP <Peter.xli@outlook.com>
2022-11-24 22:33:47 -0700

    add styles for the main element

--- stockoverwatch/src/components/support/Support.jsx
+++ stockoverwatch/src/components/support/Support.jsx
@@ -5,53 +5,61 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 
 function Support() {
   return (
-    <div className="support-container">
-      <div className="contactinfo">
-        <div>
-          <h2>Contact Info</h2>
-          <ul className="info">
-            <li>
-              <span>
-                <AlternateEmailIcon /> <br />
-              </span>
-              <span>Peter.xli@outlook.com</span>
-            </li>
-            <li>
-              <span>
-                <AlternateEmailIcon /> <br />
-              </span>
-              <span>Chris.Bowyer@gmail.com</span>
-            </li>
-            <li>
-              <span>
-                <AlternateEmailIcon /> <br />
-              </span>
-              <span>Bryan.Nuguyen@gmail.com</span>
-            </li>
-          </ul>
-          <ul className="sci">
-            <li>
-              <GitHubIcon />
-            </li>
-            <li>
-              <a href="https://github.com/LIZXP">Peter Li</a>
-            </li>
-            <li>
-              <GitHubIcon />
-            </li>
-            <li>
-              <a href="https://github.com/Devcab2">Chris Bowyer</a>
-            </li>
-            <li>
-              <GitHubIcon />
-            </li>
-            <li>
-              <a href="https://github.com/dungminhnguyen257">Bryan Nguyen</a>
-            </li>
-          </ul>
+    <section>
+      <div className="support-container">
+        <div className="contactinfo">
+          <div>
+            <h2>Contact Info</h2>
+            <ul className="info">
+              <li>
+                <span>
+                  <AlternateEmailIcon /> <br />
+                </span>
+                <span>Peter.xli@outlook.com</span>
+              </li>
+              <li>
+                <span>
+                  <AlternateEmailIcon /> <br />
+                </span>
+                <span>Chris.Bowyer@gmail.com</span>
+              </li>
+              <li>
+                <span>
+                  <AlternateEmailIcon /> <br />
+                </span>
+                <span>Bryan.Nuguyen@gmail.com</span>
+              </li>
+            </ul>
+            <ul className="sci">
+              <li>
+                <GitHubIcon />
+              </li>
+              <li>
+                <a href="https://github.com/LIZXP">Peter Li</a>
+              </li>
+              <li>
+                <GitHubIcon />
+              </li>
+              <li>
+                <a href="https://github.com/Devcab2">Chris Bowyer</a>
+              </li>
+              <li>
+                <GitHubIcon />
+              </li>
+              <li>
+                <a href="https://github.com/dungminhnguyen257">Bryan Nguyen</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="contactForm">
+          <h2>Send us a Message</h2>
+          <div className="formBox">
+            <div className="inpuitBox"></div>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
