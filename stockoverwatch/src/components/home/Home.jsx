afe4d328c65d4578a41b5868b7816afbcbfbab8b
LIZXP <Peter.xli@outlook.com>
2022-11-25 17:01:27 -0700

    Revert "fix some error"
    This reverts commit fe1f8eb95ff9c90a8992ae5d8daa713d94dc23d5.

--- stockoverwatch/src/components/home/Home.jsx
+++ stockoverwatch/src/components/home/Home.jsx
@@ -3,17 +3,17 @@ import Body from "../bodycontent/Body";
 import "./Home.styles.scss";
 
 function Main({ stocks }) {
-  if (!stocks) {
-    return null;
-  }
-  return (
+  // if (!stocks) {
+  //   return null;
+  // }
+  return stocks ? (
     <div className="stocks-container">
       <div className="Main">
         <Navbar stocks={stocks} />
         <Body />
       </div>
     </div>
-  );
+  ) : null;
 }
 
 export default Main;
