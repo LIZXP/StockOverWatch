f7ff17472ab7662e979acf2c1819353bbc8e332b
LIZXP <Peter.xli@outlook.com>
2022-12-04 11:42:12 -0700

    add stock icon for each of the stocks

--- stockoverwatch/src/helper/helperFunctions.js
+++ stockoverwatch/src/helper/helperFunctions.js
@@ -1,7 +1,32 @@
 /* eslint-disable */
 import axios from "axios";
 export const stocksPriceDataFinn = () => {
-  const stocksSymbols = ["AAPL", "META", "GOOGL", "NFLX", "AMZN", "TSLA"];
+  const stocksSymbols = [
+    {
+      name: "AAPL",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Apple_Computer_Logo_rainbow.svg/879px-Apple_Computer_Logo_rainbow.svg.png",
+    },
+    {
+      name: "META",
+      img: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/meta-icon.png",
+    },
+    {
+      name: "GOOGL",
+      img: "https://cdn-icons-png.flaticon.com/512/2991/2991148.png",
+    },
+    {
+      name: "NFLX",
+      img: "https://cdn.iconscout.com/icon/free/png-256/netflix-3521600-2945044.png",
+    },
+    {
+      name: "AMZN",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Amazon_icon.svg/2048px-Amazon_icon.svg.png",
+    },
+    {
+      name: "TSLA",
+      img: "https://upload.wikimedia.org/wikipedia/commons/e/e8/Tesla_logo.png",
+    },
+  ];
   // const fulllist = [
   //   "AAPL",
   //   "META",
@@ -38,9 +63,9 @@ export const stocksPriceDataFinn = () => {
   };
   stocksSymbols.map((ssymb) => {
     allPromise.push(
-      getStocksData(ssymb)
+      getStocksData(ssymb.name)
         .then((res) => {
-          return { ...res.data, symbol: ssymb };
+          return { ...res.data, symbol: ssymb.name, img: ssymb.img };
         })
         .catch((e) => console.log(e))
     );
@@ -49,7 +74,32 @@ export const stocksPriceDataFinn = () => {
 };
 
 export const monthStockPriceFinn = () => {
-  const stocksSymbols = ["AAPL", "META", "GOOGL", "NFLX", "AMZN", "TSLA"];
+  const stocksSymbols = [
+    {
+      name: "AAPL",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Apple_Computer_Logo_rainbow.svg/879px-Apple_Computer_Logo_rainbow.svg.png",
+    },
+    {
+      name: "META",
+      img: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/meta-icon.png",
+    },
+    {
+      name: "GOOGL",
+      img: "https://cdn-icons-png.flaticon.com/512/2991/2991148.png",
+    },
+    {
+      name: "NFLX",
+      img: "https://cdn.iconscout.com/icon/free/png-256/netflix-3521600-2945044.png",
+    },
+    {
+      name: "AMZN",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Amazon_icon.svg/2048px-Amazon_icon.svg.png",
+    },
+    {
+      name: "TSLA",
+      img: "https://upload.wikimedia.org/wikipedia/commons/e/e8/Tesla_logo.png",
+    },
+  ];
   const baseUrl = "https://finnhub.io/api/v1/stock/candle";
   const token = "cdmjq6aad3ibvooj3ue0cdmjq6aad3ibvooj3ueg";
   const UNIXtsFrom = "1666391811";
@@ -66,9 +116,9 @@ export const monthStockPriceFinn = () => {
   };
   stocksSymbols.map((ssymb) => {
     allPromiseCandle.push(
-      getStockPriceData(ssymb)
+      getStockPriceData(ssymb.name)
         .then((res) => {
-          return { ...res.data, symbol: ssymb };
+          return { ...res.data, symbol: ssymb.name, img: ssymb.img };
         })
         .catch((e) => console.log(e))
     );
