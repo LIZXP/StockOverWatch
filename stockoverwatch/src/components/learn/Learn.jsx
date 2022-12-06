33c540382a56f6f2a2d7dec409190c934810a6ef
dungminhnguyen257 <nguy7670@mylaurier.ca>
2022-12-05 23:56:09 -0500

    creating chat box

--- stockoverwatch/src/components/learn/Learn.jsx
+++ stockoverwatch/src/components/learn/Learn.jsx
@@ -1,50 +1,53 @@
-import React from 'react';
+import React from "react";
 import "./Learn.scss";
-import Card from './card/card';
-import investor1 from "../../assets/warren-buffett.jpg"
-import investor2 from "../../assets/mark-cuban.jpeg"
-import investor3 from "../../assets/barbara-corcoran.jpeg"
-
+import Card from "./card/card";
+import investor1 from "../../assets/warren-buffett.jpg";
+import investor2 from "../../assets/mark-cuban.jpeg";
+import investor3 from "../../assets/barbara-corcoran.jpeg";
+import chatbox from "./chatbox/chatbox";
 // import Typewriter from "typewriter-effect";
 
 const cards = [
-  {id:"1",
-  name: "Warren Buffet",
-  link:"https://finance.yahoo.com/news/8-genius-investing-tips-warren-120011793.html#:~:text=Invest%20In%20Low%2DCost%20Index,swears%20by%20one%20simple%20tip.&text=%22Consistently%20buy%20an%20S%26P%20500,practically%20all%20of%20the%20time.%22",
-  img: investor1,
-  quote: "If you aren't willing to own a stock for 10 years, don't even think about owning it for 10 minutes.",
-  networth: "$108.2 billions"
-},
-{id:"2",
-name: "Mark Cuban",
-link:"https://finance.yahoo.com/news/mark-cuban-top-investing-advice-193955063.html",
-img: investor2,
-quote: "I believe that starting any business should be as easy as a 10-year-old starting a lemonade stand.",
-networth: "$4.8 billions"
-},
+  {
+    id: "1",
+    name: "Warren Buffet",
+    link: "https://finance.yahoo.com/news/8-genius-investing-tips-warren-120011793.html#:~:text=Invest%20In%20Low%2DCost%20Index,swears%20by%20one%20simple%20tip.&text=%22Consistently%20buy%20an%20S%26P%20500,practically%20all%20of%20the%20time.%22",
+    img: investor1,
+    quote:
+      "If you aren't willing to own a stock for 10 years, don't even think about owning it for 10 minutes.",
+    networth: "$108.2 billions",
+  },
+  {
+    id: "2",
+    name: "Mark Cuban",
+    link: "https://finance.yahoo.com/news/mark-cuban-top-investing-advice-193955063.html",
+    img: investor2,
+    quote:
+      "I believe that starting any business should be as easy as a 10-year-old starting a lemonade stand.",
+    networth: "$4.8 billions",
+  },
 
-  {id:"3",
-  name: "Barbara Corcoran",
-  link:"https://www.inc.com/graham-winfrey/barbara-corcoran-shark-tank-business-plan-rent.html",
-  img: investor3,
-  quote: "Don’t get cocky just because you’ve had a good run. You can lose your business as fast as you’ve earned it.",
-  networth: "$100 millions"
-}
-
-]
+  {
+    id: "3",
+    name: "Barbara Corcoran",
+    link: "https://www.inc.com/graham-winfrey/barbara-corcoran-shark-tank-business-plan-rent.html",
+    img: investor3,
+    quote:
+      "Don’t get cocky just because you’ve had a good run. You can lose your business as fast as you’ve earned it.",
+    networth: "$100 millions",
+  },
+];
 export default function Learn() {
-  return <>
-  <div className='services'>
-  
-  {/* <div className="pop-out-card with-transform"> */}
-
-  <div className="flippable-card-container">
-    {cards.map((card)=>(
-
-      <Card key={card.id} card={card} />
-    ))}
-  
-  </div>
-  </div>
-  </>
+  return (
+    <>
+      <div className="services">
+        <div className="flippable-card-container">
+          {cards.map((card) => (
+            <Card key={card.id} card={card} />
+          ))}
+        </div>
+      </div>
+      <chatbox />
+    </>
+  );
 }
--- stockoverwatch/src/components/learn/card/card.jsx
+++ stockoverwatch/src/components/learn/card/card.jsx
@@ -2,21 +2,20 @@ import "./card.scss";
 import Typewriter from "typewriter-effect";
 
 export default function Card({ card }) {
-  
   return (
     <div className="card">
       <div className="card-front">
-        <img className="image" src={card.img}/>
+        <img className="image" src={card.img} />
         <div className="content">
           <p>&ldquo;{card.quote}&rdquo;</p>
-          
-       
           <strong>&mdash; {card.name},</strong> networth {card.networth}
         </div>
       </div>
       <div className="card-back">
         <a href={card.link}>
-        <Typewriter
+          {" "}
+          Learn More
+          {/* <Typewriter
           options={{
             strings:["Learn more"
           ],
@@ -24,7 +23,7 @@ export default function Card({ card }) {
           loop: true,
         }}
         
-        />
+        /> */}
         </a>
       </div>
     </div>
